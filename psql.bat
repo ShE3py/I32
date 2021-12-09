acd5d7b9b6b2bb19e9a72ea620764aac66aabff4
ShE3py <52315535+she3py@users.noreply.github.com>
2021-12-09 03:58:15 +0100

    Fix psql.bat

--- psql.bat
+++ psql.bat
@@ -3,7 +3,7 @@ title psql.exe
 echo Restore database
 echo.
 
-"C:\Program Files\PostgreSQL\14\bin\psql.exe" --set ON_ERROR_STOP=on -d postgres -U postgres -f database.dump
+"C:\Program Files\PostgreSQL\14\bin\psql.exe" -d postgres -U postgres -f database.dump
 
 echo.
 pause
