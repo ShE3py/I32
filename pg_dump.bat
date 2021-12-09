0cd2b7e561aa17839b31222c73bf7bc35cfe0e7f
ShE3py <52315535+she3py@users.noreply.github.com>
2021-12-09 06:49:28 +0100

    Fix database restore

--- pg_dump.bat
+++ pg_dump.bat
@@ -3,7 +3,7 @@ title pg_dump.exe
 echo Dump database
 echo.
 
-"C:\Program Files\PostgreSQL\14\bin\pg_dump.exe" -d postgres -U postgres -f database.dump
+"C:\Program Files\PostgreSQL\14\bin\pg_dump.exe" --blobs --clean --create -d postgres -U postgres -f database.dump
 
 echo.
 pause
